09e3369d846d7a4eaeaf7d623628c275e0be2756
unknowndev <snowboarder1541@gmail.com>
2014-05-25 20:06:08 -0700

    Added MarkedUp for tracking analytics and app crashes

---
+++ CoolFish/CoolFish/Targets/MarkedUpTarget.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+using System.Threading;
+using CoolFishNS.Utilities;
+using MarkedUp;
+using NLog;
+using NLog.Common;
+using NLog.Targets;
+using LogLevel = NLog.LogLevel;
+
+namespace CoolFishNS.Targets
+{
+    public class MarkedUpTarget : TargetWithLayout
+    {
+        public MarkedUpTarget()
+        {
+            if (!Analytics.MarkedUp.IsInitialized)
+            {
+                Analytics.MarkedUp.Initialize();
+            }
+        }
+
+        private void SetThreadCulture()
+        {
+            // Change culture under which this application runs
+            var ci = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+
+        protected override void Write(AsyncLogEventInfo[] logEvents)
+        {
+            foreach (AsyncLogEventInfo logEvent in logEvents)
+            {
+                Write(logEvent);
+            }
+        }
+
+        protected override void Write(AsyncLogEventInfo logEvent)
+        {
+            Write(logEvent.LogEvent);
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            if (logEvent.Level == LogLevel.Error)
+            {
+                SetThreadCulture();
+                AnalyticClient.Error(logEvent.FormattedMessage, logEvent.Exception);
+            }
+            else if (logEvent.Level == LogLevel.Fatal)
+            {
+                SetThreadCulture();
+                AnalyticClient.Fatal(logEvent.FormattedMessage, logEvent.Exception);
+            }
+        }
+    }
+}
